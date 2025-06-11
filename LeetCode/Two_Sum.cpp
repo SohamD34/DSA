c98beb8207602f064daa69a8ff279db8219a9fa2
SohamD34 <deshmukh.2@iitj.ac.in>
2025-06-11 20:08:20 +0530

    Added and edited latest submissions

--- LeetCode/Two_Sum.cpp
+++ LeetCode/Two_Sum.cpp
@@ -3,39 +3,51 @@
 #include <unordered_map>
 using namespace std;
 
-vector<int> TwoSum(vector<int> &nums, int target){
+'''
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
 
+Example:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+'''
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    
     unordered_map<int, int> M;
-    int n = nums.size();
-    vector<int> v;
+    vector<int> ans;
 
-    for(int i=0; i<n; i++){
-        M[nums[i]] = i+1;
-    }
-
-    for(int i=0; i<n; i++){
-
-        if(M[target - nums[i]] && (M[target - nums[i]]-1 != i)){
-            v.push_back(i);
-            v.push_back(M[target - nums[i]]-1);
+    for(int i=0; i<nums.size(); ++i){
+        
+        if(M.find(target-nums[i]) != M.end()){
+            ans.push_back(M[target-nums[i]]-1);
+            ans.push_back(i);
             break;
         }
+        else{
+            M[nums[i]] = i+1;
+        }
     }
-
-    return v; 
-
+    return ans;
 }
-
 
 int main(){
 
-    vector<int> nums = {3, 2, 4};
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-    vector<int> ans = TwoSum(nums, 6);
-    for(auto i:ans){
-        cout<<i<<" ";
+    vector<int> nums = {2, 7, 13, 11, 15};
+    int target = 15;
+
+    vector<int> ans = twoSum(nums, target);
+
+    for(auto i: ans){
+        cout << i << " ";
     }
-    cout<<endl;
-    
+
     return 0;
 }
